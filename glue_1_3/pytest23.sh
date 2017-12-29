289a56d71bd63be22158aced164969c497cc89bd
iszulc <iszulc@relayonit.pl>
2017-12-29 15:16:11 +0100

    move glue versions to folders

---
+++ glue_1_3/pytest23.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+MODULE_NAME=glue_1_3
+
+echo 'pytest + coverage: Python 2...'
+python2 -m coverage run --source ${MODULE_NAME} -m pytest -v
+echo 'pytest + coverage: Python 3...'
+python3 -m coverage run --source ${MODULE_NAME} -m pytest -v
+
+coverage report -m
