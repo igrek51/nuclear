0d4014039c8f281118ec968932824fc1e35eb426
igrek51 <igrek51.dev@gmail.com>
2019-07-06 23:34:12 +0200

    Release version 0.2.3

--- publish.sh
+++ publish.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-python3 setup.py sdist bdist_wheel
 
-python3 -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
-python3 -m twine upload dist/*
+#clean
+rm -rf build/
+rm -rf dist/
+
+python3.6 setup.py sdist bdist_wheel
+
+#python3.6 -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+python3.6 -m twine upload dist/*
 
