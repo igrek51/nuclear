bd3966794b274cc39ceb1f85429be68f18bebf32
igrek51 <igrek51.dev@gmail.com>
2023-04-04 21:37:12 +0200

    Add option to show full docs and values

--- nuclear/inspection/inspection.py
+++ nuclear/inspection/inspection.py
@@ -7,6 +7,7 @@ from typing import Any, List, Optional, Type, Iterable
 class InspectConfig:
     attrs: bool
     dunder: bool
+    full: bool
 
 
 @dataclass
@@ -23,22 +24,32 @@ class InspectAttribute:
 
 def inspect(
     obj: Any,
+    *,
     attrs: bool = True,
     dunder: bool = False,
+    full: bool = False,
 ):
-    """Inspect object type and value and print its variables and methods"""
-    print(inspect_format(obj, attrs=attrs, dunder=dunder))
+    """
+    Inspect object type and value and print its variables and methods
+    :param obj: object to inspect
+    :param attrs: whether to print attributes (variables and methods)
+    :param dunder: whether to print dunder attributes
+    :param full: whether to print non-abbreviated docs and values
+    """
+    print(inspect_format(obj, attrs=attrs, dunder=dunder, full=full))
 
 
 ins = inspect  # alias
 
 
-def inss(obj: Any):  # inspect short
-    inspect(obj, attrs=False, dunder=False)
+def inss(obj: Any, **kwargs):
+    """Inspect short"""
+    inspect(obj, attrs=False, **kwargs)
 
 
-def insa(obj: Any):  # inspect all
-    inspect(obj, attrs=True, dunder=True)
+def insa(obj: Any, **kwargs):
+    """Inspect all"""
+    inspect(obj, attrs=True, dunder=True, **kwargs)
 
 
 def inspect_format(
@@ -46,25 +57,26 @@ def inspect_format(
     *,
     attrs: bool = True,
     dunder: bool = False,
+    full: bool = False,
 ) -> str:
-    config = InspectConfig(attrs=attrs, dunder=dunder)
+    config = InspectConfig(attrs=attrs, dunder=dunder, full=full)
 
     output: List[str] = [
         f'str: {str(obj)}',
         f'type: {type(obj)}',
     ]
-    doc = _get_doc(obj)
+    doc = _get_doc(obj, config)
     if doc:
         output.append(f'doc: {doc}')
 
     if config.attrs:
-        attributes = sorted(_iter_attributes(obj), key=lambda attr: attr.name)
+        attributes = sorted(_iter_attributes(obj, config), key=lambda attr: attr.name)
         output.extend(_format_attrs_section(attributes, config))
 
     return '\n'.join(line for line in output if line is not None)
 
 
-def _iter_attributes(obj: Any) -> Iterable[InspectAttribute]:
+def _iter_attributes(obj: Any, config: InspectConfig) -> Iterable[InspectAttribute]:
     keys = dir(obj)
     for key in keys:
         value = getattr(obj, key)
@@ -72,7 +84,7 @@ def _iter_attributes(obj: Any) -> Iterable[InspectAttribute]:
         dunder = key.startswith('__') and key.endswith('__')
         private = key.startswith('_') and not dunder
         signature = _get_callable_signature(key, value) if callable_ else None
-        doc = _get_doc(value) if callable_ else None
+        doc = _get_doc(value, config) if callable_ else None
         yield InspectAttribute(
             name=key,
             value=value,
@@ -101,15 +113,11 @@ def _get_callable_signature(name: str, obj: Any) -> Optional[str]:
     return f'{prefix} {name}{_signature}'
 
 
-def _get_doc(obj: Any) -> Optional[str]:
+def _get_doc(obj: Any, config: InspectConfig) -> Optional[str]:
     doc = std_inspect.getdoc(obj)
     if doc is None:
         return None
-    doc = doc.strip()
-    first_line, _, _ = doc.partition('\n')
-    if len(first_line) > 100:
-        first_line = first_line[:100] + '...'
-    return first_line
+    return _shorten_string(doc.strip(), config)
 
 
 def _format_attrs_section(attributes: List[InspectAttribute], config: InspectConfig) -> Iterable[str]:
@@ -128,7 +136,7 @@ def _format_attrs_section(attributes: List[InspectAttribute], config: InspectCon
         yield ""
         yield "Public attributes:"
         for attr in public_vars:
-            yield _format_attr_variable(attr)
+            yield _format_attr_variable(attr, config)
         if public_vars and public_methods:
             yield ""
         for attr in public_methods:
@@ -138,7 +146,7 @@ def _format_attrs_section(attributes: List[InspectAttribute], config: InspectCon
         yield ""
         yield "Private attributes:"
         for attr in private_vars:
-            yield _format_attr_variable(attr)
+            yield _format_attr_variable(attr, config)
         if private_vars and private_methods:
             yield ""
         for attr in private_methods:
@@ -148,24 +156,36 @@ def _format_attrs_section(attributes: List[InspectAttribute], config: InspectCon
         yield ""
         yield "Dunder attributes:"
         for attr in dunder_vars:
-            yield _format_attr_variable(attr)
+            yield _format_attr_variable(attr, config)
         if dunder_vars and dunder_methods:
             yield ""
         for attr in dunder_methods:
             yield _format_attr_method(attr)
 
 
-def _format_attr_variable(attr: InspectAttribute) -> str:
-    value_str = str(attr.value)
-    if len(value_str) > 100:
-        value_str = value_str[:100] + '...'
+def _format_attr_variable(attr: InspectAttribute, config: InspectConfig) -> str:
+    value_str = _shorten_string(str(attr.value), config)
+    if isinstance(attr.value, str):
+        value_str = f"'{value_str}'"
     return f'  {attr.name}: {attr.type} = {value_str}'
 
 
 def _format_attr_method(attr: InspectAttribute) -> str:
     if not attr.signature:
-        return f'  {attr.name}(...)'
+        return f'  {attr.name}(…)'
     if attr.doc:
-        return f'  {attr.signature}: {attr.doc}'
+        return f'  {attr.signature}: # {attr.doc}'
     else:
         return f'  {attr.signature}'
+
+
+def _shorten_string(text: str, config: InspectConfig) -> str:
+    if config.full:
+        return text
+    
+    first_line, _, rest = text.partition('\n')
+    if rest:
+        first_line = first_line + '…'
+    if len(first_line) > 100:
+        first_line = first_line[:100] + '…'
+    return first_line
