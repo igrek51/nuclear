469b0a73091b499d8d800a45f08dc6977af1a638
igrek51 <igrek51.dev@gmail.com>
2024-01-28 21:37:12 +0100

    Add time conversion tools

--- nuclear/utils/strings.py
+++ nuclear/utils/strings.py
@@ -1,12 +1,12 @@
-from typing import List, Union
+from typing import List, Union, Optional
 
 
-def nonempty_lines(str_in):
+def nonempty_lines(str_in: str) -> list[str]:
     all_lines = str_in.splitlines()
     return list(filter(lambda l: len(l) > 0, all_lines))
 
 
-def split_to_tuple(line, attrs_count=None, splitter='\t'):
+def split_to_tuple(line: str, attrs_count: Optional[int] = None, splitter: str = '\t'):
     parts = line.split(splitter)
     if attrs_count and len(parts) != attrs_count:
         raise RuntimeError(f'invalid split parts count (found: {len(parts)}, expected: {attrs_count}) in line: {line}')
@@ -17,3 +17,17 @@ def split_to_tuples(lines: Union[List[str], str], attrs_count=None, splitter='\t
     if not isinstance(lines, list):
         lines = nonempty_lines(lines)
     return list(map(lambda line: split_to_tuple(line, attrs_count, splitter), lines))
+
+
+def format_bytes(count: int):
+    """Format bytes amount as a human friendly string with KiB, MiB, GiB"""
+    if count == 1:
+        return '1 byte'
+    elif count < 1024:
+        return f'{count} bytes'
+    elif count < 1024**2:
+        return f'{count/1024:.2f} KiB'
+    elif count < 1024**3:
+        return f'{count/1024/1024:.2f} MiB'
+    else:
+        return f'{count/1024/1024/1024:.2f} GiB'
--- nuclear/utils/time.py
+++ nuclear/utils/time.py
@@ -1,16 +1,95 @@
-import datetime
+from datetime import datetime, timezone
+
+PARSE_DATE_FORMATS = [
+    '%Y-%m-%dT%H:%M:%S.%fZ',
+    '%Y-%m-%dT%H:%M:%SZ',
+    '%Y-%m-%d %H:%M:%S',
+    '%Y-%m-%d',
+]
 
 
-def str2time(time_raw, pattern):
-    """pattern: %Y-%m-%d, %H:%M:%S"""
-    try:
-        return datetime.datetime.strptime(time_raw, pattern)
-    except ValueError as _:
-        return None
+def now() -> datetime:
+    """Return current datetime with UTC timezone set"""
+    return datetime.now(timezone.utc)
 
 
-def time2str(date_time, pattern):
-    """pattern: %Y-%m-%d, %H:%M:%S"""
-    if not date_time:
-        return None
-    return date_time.strftime(pattern)
+def now_timestamp() -> int:
+    return datetime_to_timestamp(now())
+
+
+def datetime_to_timestamp(dt: datetime) -> int:
+    """Convert datetime.datetime to integer timestamp in seconds"""
+    return int(dt.timestamp())
+
+
+def timestamp_to_datetime(timestamp: int) -> datetime:
+    """Convert integer timestamp in seconds to datetime.datetime"""
+    return datetime.fromtimestamp(timestamp, tz=timezone.utc)
+
+
+def datetime_to_str(dt: datetime) -> str:
+    """Convert datetime to ISO 8601 format"""
+    return dt.strftime('%Y-%m-%dT%H:%M:%S%z')
+
+
+def date_to_str(dt: datetime) -> str:
+    return dt.strftime('%Y-%m-%d')
+
+
+def str_iso8601_to_datetime(text: str) -> datetime:
+    return datetime.strptime(text, '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=timezone.utc)
+
+
+def parse_date(text: str) -> datetime:
+    for date_format in PARSE_DATE_FORMATS:
+        try:
+            return datetime.strptime(text, date_format).replace(tzinfo=timezone.utc)
+        except ValueError:
+            pass
+    raise ValueError(f'cannot parse {text} as date')
+
+
+def str_to_timestamp_ms(text: str) -> int:
+    dt = parse_date(text)
+    return int(dt.timestamp() * 1000)
+
+
+def timestamp_pretty_ago(timestamp: int) -> str:
+    """
+    Convert past date to user-friendly description compared to current datetime.
+    eg.: 'an hour ago', 'yesterday', '3 months ago', 'just now'
+    """
+    diff = now() - timestamp_to_datetime(timestamp)
+    second_diff: int = diff.seconds
+    day_diff: int = diff.days
+
+    if day_diff < 0:
+        return ''
+    if day_diff == 0:
+        if second_diff < 10:
+            return "just now"
+        if second_diff < 60:
+            return f"{second_diff} seconds ago"
+        if second_diff < 120:
+            return "a minute ago"
+        if second_diff < 3600:
+            return f"{second_diff // 60} minutes ago"
+        if second_diff < 7200:
+            return "an hour ago"
+        if second_diff < 86400:
+            return f"{second_diff // 3600} hours ago"
+    if day_diff == 1:
+        return "yesterday"
+    if day_diff < 7:
+        return f"{day_diff} days ago"
+    if day_diff // 7 == 1:
+        return f"a week ago"
+    if day_diff < 31:
+        return f"{day_diff // 7} weeks ago"
+    if day_diff // 30 == 1:
+        return f"a month ago"
+    if day_diff < 365:
+        return f"{day_diff // 30} months ago"
+    if day_diff // 365 == 1:
+        return f"a year ago"
+    return f"{day_diff // 365} years ago"
