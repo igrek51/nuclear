8684f0604c1bbe4edde1f5e1e65de48051eef75e
igrek51 <igrek51.dev@gmail.com>
2025-11-26 00:06:42 +0100

    Add stub for invoking parse_cli_args

--- nuclear/nuke/invoke.py
+++ nuclear/nuke/invoke.py
@@ -1,5 +1,6 @@
 import inspect
 import sys
+from typing import Any
 
 from nuclear.sublog import logger, error_handler
 
@@ -11,7 +12,11 @@ def run():
 
 def _run_with_args(args: list[str]):
     if not args:
-        return _list_targets()
+        return _show_available_targets()
+
+    positional, overrides = parse_cli_args(args)
+
+    config = _get_config_object()
 
     function_names: list[str] = _list_target_names()
     for arg in args:
@@ -24,7 +29,28 @@ def _run_with_args(args: list[str]):
         function()
 
 
-def _list_targets():
+def parse_cli_args(args: list[str]) -> tuple[list[str], dict[str, str]]:
+    """
+    Extract CLI parameters in the format:
+        --name=value goes as {'name': 'value'} in the overrides dict
+        --param-name is extracted as {'param_name': '1'} in the overrides dict - interpreted as flag
+        --long-name="long value" is extracted as {'long_name': 'long value'}
+    Once the parameter or flag is detected, it is dropped from the list.
+    The rest that stays at the end, are the positional arguments.
+    Return tuple of positional arguments and the dict of extracted parameters / flags.
+    """
+    return [], {}
+
+
+def _get_config_object() -> Any:
+    main_module = sys.modules['__main__']
+    if not hasattr(main_module, 'config'):
+        logger.warn("Can't find config object in the main module")
+        return {}
+    return getattr(main_module, 'config')
+
+
+def _show_available_targets():
     function_names: list[str] = _list_target_names()
     if not function_names:
         logger.warn('No available target functions - add public function in the main module')
